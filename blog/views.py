d58b0288eba8f976a5db3fd1ea204913647e415f
Domon <viplizhao@gmail.com>
2017-11-20 18:06:05 +0800

    Add Comment model

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 import markdown
 
 from django.shortcuts import render, get_object_or_404
+from comments.form import CommentForm
 from .models import Post, Category
 
 
@@ -30,4 +31,13 @@ def detail(request, pk):
                                       'markdown.extensions.codehilite',
                                       'markdown.extensions.toc',
                                   ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+
+    context = {
+        'post': post,
+        'form': form,
+        'comment_list': comment_list
+    }
+
+    return render(request, 'blog/detail.html', context=context)
